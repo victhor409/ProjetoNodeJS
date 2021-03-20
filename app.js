1e371fde5cb6fc84521e117d3de7ffc057396781
victor <victhor409@gmail.com>
2021-03-20 13:00:12 -0300

    rota do formulario

--- app.js
+++ app.js
@@ -11,8 +11,19 @@ const sequelize = new Sequelize('test','root','123',{
 
 //config
  //tamplate handle
- app.engine('handlers',handlebars({defaultLayout: 'main'}));
+ app.engine('handlebars',handlebars({defaultLayout: 'main'}));
  app.set('view engine', 'handlebars');
+
+
+ //Rotas 
+
+ app.get('/cad', function(req, res){
+     res.render('formulario');
+ });
+
+ app.post('/add' , function(req, res){
+     res.send("Enviado");
+ })
 
 
 //função de callback 
