cb40418b18d4ac9d15ad219581651408e52dd44c
victor <victhor409@gmail.com>
2021-03-23 00:11:46 -0300

    criando banco de dados , gravando formulario no BD, redirecionando a rota para home

--- app.js
+++ app.js
@@ -1,32 +1,44 @@
 const express = require("express");
 const app = express();
 const handlebars = require('express-handlebars');
+const bodyParser = require('body-parser');
+const Post = require( './models/Posts');
 
-//conexao com BD
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize('test','root','123',{
-    host:"localhost",
-    dialect:"mysql"
-});
 
 //config
  //tamplate handle
  app.engine('handlebars',handlebars({defaultLayout: 'main'}));
  app.set('view engine', 'handlebars');
+ //body parser
+ app.use(bodyParser.urlencoded({extended: false}));
+ app.use(bodyParser.json());
+
 
 
  //Rotas 
+
+app.get('/',function(req, res){
+    res.render('home')
+})
+
 
  app.get('/cad', function(req, res){
      res.render('formulario');
  });
 
  app.post('/add' , function(req, res){
-     res.send("Enviado");
- })
+     Post.create({
+         titulo: req.body.titulo,
+         conteudo: req.body.conteudo
+     }).then(function(){
+         res.redirect('/')
+     }).catch(function(erro){
+         res.send("Houve um erro "+erro)
+     })
+ });
 
 
 //função de callback 
-app.listen(8080, function(){
-    console.log("Servidor Rodando na url http://localhost:8080")
+app.listen(8082, function(){
+    console.log("Servidor Rodando na url http://localhost:8082")
 });
